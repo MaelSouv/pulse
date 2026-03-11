a5129ffb3ec03e0c0c621a7fd3e871e438e07bbd
Mathis <delasalle.sio.lebreton.m@gmail.com>
2026-03-11 11:47:58 +0100

    Ajout du swagger

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MessagesService } from './messages.service';
-import { MessagesGateway } from './messages.gateway';
+import { MessagesController } from './messages.controller';
 
 @Module({
-  providers: [MessagesGateway, MessagesService],
+  controllers: [MessagesController],
+  providers: [MessagesService],
 })
 export class MessagesModule {}
