d343a25a123ba275a439fa4c06b39656a2aed749
MaelSouv <souvestremael@gmail.com>
2026-03-11 11:22:32 +0100

    Ajout auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,11 @@ import { EventsModule } from './events/events.module';
 import { RidesModule } from './rides/rides.module';
 import { MessagesModule } from './messages/messages.module';
 import { HousingModule } from './housing/housing.module';
+import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, EventsModule, RidesModule, MessagesModule, HousingModule],
+  imports: [UsersModule, EventsModule, RidesModule, MessagesModule, HousingModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
