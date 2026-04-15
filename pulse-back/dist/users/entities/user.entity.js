4e2c3f1612b98931c35e12ecfa1dcc1016a11efa
MaelSouv <souvestremael@gmail.com>
2026-04-15 14:14:40 +0200

    Nettoyage : application du gitignore pour dist et node_modules

--- pulse-back/dist/users/entities/user.entity.js
+++ pulse-back/dist/users/entities/user.entity.js
@@ -23,7 +23,7 @@ __decorate([
     __metadata("design:type", String)
 ], Users.prototype, "nom", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ name: 'email', unique: true }),
+    (0, typeorm_1.Column)({ name: 'email', unique: true, length: 190 }),
     __metadata("design:type", String)
 ], Users.prototype, "email", void 0);
 __decorate([
